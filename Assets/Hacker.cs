374ae4a6d27805f031cd11a290b00787c49c50f9
Bruno Costa Pinheiro <b3dsign@gmail.com>
2019-03-08 13:16:58 +0000

    Setting passwords

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -9,6 +9,7 @@ public class Hacker : MonoBehaviour
     int level;
     enum Screen { MainMenu, Password, Win };
     Screen currentScreen;
+    string password;
 
     // Start is called before the first frame update
     void Start()
@@ -32,26 +33,47 @@ public class Hacker : MonoBehaviour
     {
         if (input == "menu") // we can always go to the main menu
         {
-            ShowMainMenu();
-            
+            ShowMainMenu();            
         }
         else if (currentScreen == Screen.MainMenu)
         {
             RunMainMenu(input);
         }
+        else if (currentScreen == Screen.Password)
+        {
+            TestPass(input);
+        }
     }
 
     void RunMainMenu(string input)
     {
+        switch (input)
+        {
+            case "1":
+                level = 1;
+                StartGame(input);
+                break;
+            case "2":
+                level = 2;
+                StartGame(input);
+                break;
+            case "007":
+                Terminal.WriteLine("Hi Mr. Bond! Please select a level.");
+                break;
+            default:
+                Terminal.WriteLine("Wrong! Please choose a valid level.");
+                break;
+        }
+        /*
         if (input == "1")
         {
             level = 1;
-            StartGame();
+            StartGame(input);
         }
         else if (input == "2")
         {
             level = 2;
-            StartGame();
+            StartGame(input);
         }
         else if (input == "007")
         {
@@ -60,14 +82,42 @@ public class Hacker : MonoBehaviour
         else
         {
             Terminal.WriteLine("Wrong! Please choose a valid level.");
-        }
+        }*/
     }
 
-    void StartGame()
+    void StartGame(string input)
     {
         currentScreen = Screen.Password;
         Terminal.WriteLine("The level " + level + " has been chosen");
         Terminal.WriteLine("Please type the Password:");
     }
 
+    private void TestPass(string input)
+    {
+        if (level == 1)
+        {
+            if (input == "one")
+            {
+                Terminal.WriteLine("Well Done!");
+            }
+            else
+            {
+                Terminal.WriteLine("Sorry, your password is wrong!");
+                Terminal.WriteLine("Please type the Password:");
+            }
+
+        }
+        else if (level == 2)
+        {
+            if (input == "two")
+            {
+                Terminal.WriteLine("Well Done!");
+            }
+            else
+            {
+                Terminal.WriteLine("Sorry, your password is wrong!");
+                Terminal.WriteLine("Please type the Password:");
+            }
+        }
+    }
 }
